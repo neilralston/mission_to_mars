773bb7035ee671e59a78b28feddcceb0bb888f7a
NRalston <Neil.Ralston@MSPMAC.ORG>
2022-05-29 15:50:40 -0500

    through del 2

--- scraping.py
+++ scraping.py
@@ -22,7 +22,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_image_urls
     }
  
     # Stop webdriver and return data
@@ -101,6 +102,27 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    imgs_links = browser.find_by_css("a.itemLink.product-item h3")
+
+    for i in range(4):
+        hemispheres = {}
+        browser.find_by_css('a.itemLink.product-item h3')[i].click()
+        element = browser.find_link_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css('h2.title').text
+        hemispheres["img_url"] = img_url
+        hemispheres["title"] = title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+
+    return hemisphere_image_urls
 
 if __name__ == "__main__":
 
